c177ac944832266a120e7c01ae39f2333830b335
25Priyanka27 <priyankatamsetwar@gmail.com>
2026-03-02 16:07:59 +0530

    Update p-star.py

--- p-star.py
+++ p-star.py
@@ -1,5 +1,6 @@
-n=5
-for i in range(n)
-spaces=n-i-1
-stars=2*i+1
-print(""* spaces+"*" stars)
+n = 5
+
+for i in range(n):
+    spaces = n - i - 1
+    stars = 2 * i + 1
+    print(" " * spaces + "*" * stars)
